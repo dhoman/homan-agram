3a991ce27a672f61811ffc3323af9c35914efa2e
dhoman <dhoman@huskers.unl.edu>
2018-05-21 01:21:01 -0600

    basic setup, still doesn't work

---
+++ config.js
@@ -0,0 +1,6 @@
+var settings = {
+  "user_name": process.env.INSTAGRAM_USER ? process.env.INSTAGRAM_USER : "YOUR_INSTAGRAM_USER",
+  "password": process.env.INSTAGRAM_PASS ? process.env.INSTAGRAM_PASS : "YOUR_INSTAGRAM_PASS",
+};
+
+module.exports = settings;
